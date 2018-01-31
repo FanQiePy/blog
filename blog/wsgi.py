2c51dcfbaecf654c4dd0d82f9f5d75583605c4c5
FanQiePy <1403193348@qq.com>
2018-02-01 00:40:48 +0800

    development

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -6,8 +6,18 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+import site
+
+site_packages = '/home/web_venv/lib/python3.6/site-packages'
+site.addsitedir(site_packages)
+
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(project)
+sys.path.append(os.path.join(project, 'apps'))
 
 from django.core.wsgi import get_wsgi_application
 
