0552065edefa43c6af3a9dc7b18dd000c2dae313
FanQiePy <1403193348@qq.com>
2018-03-02 23:52:15 +0800

    myself-resume

--- apps/myself/views.py
+++ apps/myself/views.py
@@ -1,7 +1,14 @@
+import os
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 
 def myself(request):
-    return render(request, 'own.html')
+    blog_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    resume_path = blog_path + '/static/resume/lvbiaobiao-python.pdf'
+    with open(resume_path, 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=lvbiaobiao-python.pdf'
+    # response = FileResponse(open(resume_path, 'rb'))
+    return response
